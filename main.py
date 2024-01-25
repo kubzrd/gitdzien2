dc1554259e88f87031d5a72a3b50b3f3e65a6ba8
Jakub Gardon <kubag960@gmail.com>
2024-01-25 18:26:09 +0100

    test

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 print("dzien drugi git")
 print("zmiana w pychartmie ")
+print("RwpMw8OXr9L1R0HaMvLDA3d/GsjS3TaNFmDewaPdJE8")
+# SHA256:RwpMw8OXr9L1R0HaMvLDA3d/GsjS3TaNFmDewaPdJE8 kubag960@gmail.com
